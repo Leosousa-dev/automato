2b36d1bb3b7b484a5310c7571263f6db8987cd8e
leonardo <leonardocasa790@gmail.com>
2023-11-20 11:52:58 -0300

    docs: defining Alphabet and states

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,16 @@
+// defining the set of states
+
 export enum States{
    q0 = "q0",
-   q1 = "q0",
-   q2 = "q0",
-   q3 = "q0",
-   q4 = "q0",
+   q1 = "q1",
+   q2 = "q2",
+   q3 = "q3",
+   q4 = "q4",
 }
+
+
+// defining the alphabet
+
+type L = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' | 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z';
+type D = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+type Alphabet = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' | 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z';
